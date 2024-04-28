e4b2d2f0c6b5c2f4347aa0ef91e078bbf658eebf
Evsin Igor <Evsin19@icloud.com>
2024-04-28 15:48:14 +0300

    editing SoftAssertiosJunitTests

---
+++ src/test/java/SoftAssertionsJunitTests.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SoftAssertionsJunitTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080"; // Разрешение браузера
+        Configuration.pageLoadStrategy = "eager"; // Параметры загрузки страницы
+    }
+    @AfterEach
+    void afterEach(){
+        closeWebDriver();
+    }
+
+    @Test
+    void softAssertionsShouldBeJuinit(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".Layout-main").find(byText("Soft assertions")).shouldBe(visible).click();
+        $$("div.markdown-heading").findBy(text("JUnit5")).sibling(0)
+                .shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+
+
+    }
+
+}
